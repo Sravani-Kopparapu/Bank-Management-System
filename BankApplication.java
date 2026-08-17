f48800664c4fe3100e26acb3d285eb47424df807
SravaniKopparapu <kopparapusravani2005@gmail.com>
2026-08-17 22:10:28 +0530

    Using swtich case and loops

--- BankApplication.java
+++ BankApplication.java
@@ -21,20 +21,56 @@ class BankApplication {
         double openingBalance = sc.nextDouble();
 
         Bank bank = new Bank(bankName, userId, userName, accountNumber, accountType, openingBalance);
-        bank.displayBankDetails();
-        bank.displayUserDetails();
-        bank.displayAccountDetails();
-        System.out.println("Account Summary");
-        String summary = bank.createAccountSummary();
-        System.out.println(summary);
-        System.out.println("Enter Minimum Balance: ");
-        double minimumBalance = sc.nextDouble();
-        boolean result = bank.hasMinimumBalance(minimumBalance);
-        if (result == true) {
-            System.out.println("Minimum balance is maintained");
-        } else {
-            System.out.println("Minimum balance is not maintained");
-        }
+        do {
+            System.out.println();
+            System.out.println("===============Menu================");
+            System.out.println();
+            System.out.println("1. Display Bank Details");
+            System.out.println("2. Display User Details");
+            System.out.println("3. Display Account Details");
+            System.out.println("4. Display Account Summary");
+            System.out.println("5. Check if Minimum Balance is Maintained");
+            System.out.println("6. Exit");
+            System.out.println();
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    bank.displayBankDetails();
+                    break;
+                case 2:
+                    bank.displayUserDetails();
+                    break;
+                case 3:
+                    bank.displayAccountDetails();
+                    break;
+                case 4:
+                    String summary = bank.createAccountSummary();
+                    System.out.println(summary);
+                    break;
+                case 5:
+                    System.out.println("Enter Minimum Balance: ");
+                    double minimumBalance = sc.nextDouble();
+                    boolean result = bank.hasMinimumBalance(minimumBalance);
+                    if (result == true) {
+                        System.out.println("Minimum balance is maintained");
+                    } else {
+                        System.out.println("Minimum balance is not maintained");
+                    }
+                    break;
+                case 6:
+                    System.out.println("Thank you for using our banking service. Exiting the application.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again with a valid number between 1 to 6");
+                    break;
+            }
+            if (choice == 6) {
+                break;
+            }
+        } while (true);
+
         sc.close();
     }
 }
